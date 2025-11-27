937af0cdf9c32e5b41be3e2136064c80740606af
aishieee <157057191+aishieee@users.noreply.github.com>
2025-11-27 22:34:01 +0000

    import altair for statistical visualisations

--- app.py
+++ app.py
@@ -49,6 +49,7 @@ rf_model = load_model()
 # -----------------------------------------------------
 import numpy as np
 import pandas as pd
+import altair as alt
 
 MODEL_FEATURES = [
     "GarageArea",
